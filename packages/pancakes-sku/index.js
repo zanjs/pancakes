280923ff8a8db2c7756edce3e276a7449cdd0571
zanjs <root@zanjs.com>
2018-01-22 12:57:35 +0800

    sku install script

--- packages/pancakes-sku/index.js
+++ packages/pancakes-sku/index.js
@@ -1,3 +1,57 @@
+const getFlagArrs = (m, n) => {
+  if (!n || n < 1) {
+    return []
+  }
+  const resultArrs = []
+  const flagArr = []
+  let isEnd = false
+  let leftCnt
+  for (let i = 0; i < m; i++) {
+    flagArr[i] = i < n ? 1 : 0
+  }
+  resultArrs.push(flagArr.concat())
+  while (!isEnd) {
+    leftCnt = 0
+    for (let i = 0; i < m - 1; i++) {
+      if (flagArr[i] === 1 && flagArr[i + 1] === 0) {
+        for (let j = 0; j < i; j++) {
+          flagArr[j] = j < leftCnt ? 1 : 0
+        }
+        flagArr[i] = 0
+        flagArr[i + 1] = 1
+        const aTmp = flagArr.concat()
+        resultArrs.push(aTmp)
+        if (aTmp.slice(-n).join('').indexOf('0') === -1) {
+          isEnd = true
+        }
+        break
+      }
+      flagArr[i] === 1 && leftCnt++
+    }
+  }
+  return resultArrs
+}
+
+const arrayCombine = (targetArr) => {
+  if (!targetArr || !targetArr.length) {
+    return []
+  }
+  const len = targetArr.length
+  const resultArrs = []
+  for (let n = 1; n < len; n++) {
+    const flagArrs = getFlagArrs(len, n)
+    while (flagArrs.length) {
+      const flagArr = flagArrs.shift()
+      const combArr = targetArr.reduce((combArr, m, index) => {
+        flagArr[index] && combArr.push(m)
+        return combArr
+      }, [])
+      resultArrs.push(combArr)
+    }
+  }
+  return resultArrs
+}
+
 export default {
   tag: {
     attribute: 'PropIds',
@@ -36,7 +90,7 @@ export default {
     skuKeys.forEach(skuKey => {
       const sku = data[skuKey]
       const skuKeyAttrs = skuKey.split(';')
-      const combArr = vm.arrayCombine(skuKeyAttrs)
+      const combArr = arrayCombine(skuKeyAttrs)
       for (let j = 0; j < combArr.length; j++) {
         const key = combArr[j].join(';')
         if (SKUResult[key]) {
@@ -62,59 +116,6 @@ export default {
     })
 
     return SKUResult
-  },
-  getFlagArrs (m, n) {
-    if (!n || n < 1) {
-      return []
-    }
-    const resultArrs = []
-    const flagArr = []
-    let isEnd = false
-    let leftCnt
-    for (let i = 0; i < m; i++) {
-      flagArr[i] = i < n ? 1 : 0
-    }
-    resultArrs.push(flagArr.concat())
-    while (!isEnd) {
-      leftCnt = 0
-      for (let i = 0; i < m - 1; i++) {
-        if (flagArr[i] === 1 && flagArr[i + 1] === 0) {
-          for (let j = 0; j < i; j++) {
-            flagArr[j] = j < leftCnt ? 1 : 0
-          }
-          flagArr[i] = 0
-          flagArr[i + 1] = 1
-          const aTmp = flagArr.concat()
-          resultArrs.push(aTmp)
-          if (aTmp.slice(-n).join('').indexOf('0') === -1) {
-            isEnd = true
-          }
-          break
-        }
-        flagArr[i] === 1 && leftCnt++
-      }
-    }
-    return resultArrs
-  },
-  arrayCombine (targetArr) {
-    if (!targetArr || !targetArr.length) {
-      return []
-    }
-    const vm = this
-    const len = targetArr.length
-    const resultArrs = []
-    for (let n = 1; n < len; n++) {
-      const flagArrs = vm.getFlagArrs(len, n)
-      while (flagArrs.length) {
-        const flagArr = flagArrs.shift()
-        const combArr = targetArr.reduce((combArr, m, index) => {
-          flagArr[index] && combArr.push(m)
-          return combArr
-        }, [])
-        resultArrs.push(combArr)
-      }
-    }
-    return resultArrs
   },
   /**
    * sku点击事件
